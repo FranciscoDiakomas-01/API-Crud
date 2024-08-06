9ef34649b9c3173e6062f84c26cf21908389aaad
Francisco <franciscodiakoma@gmail.com>
2024-08-06 15:28:12 +0100

    terminei

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql'
+import { dbCredentials } from './config.js'
 
 export const db = mysql.createConnection({
-    user :  process.env.UserDB "root",
-    host :  process.env.DbHost  "localhost",
-    password : process.env.DBpass || "",
-    database : "CRUDNode"
+    user :  process.env.UserDB || dbCredentials.user,
+    host :  process.env.DbHost || dbCredentials.host,
+    password : process.env.DBpass || dbCredentials.password,
+    database : process.env.DB || dbCredentials.db
 })
 db.connect((err)=>{
     if(err){
